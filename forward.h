884c013022abddf0f0c1430361e1ad205278c719
Jhonatan Macazana <jhonatan.macazana@utec.edu.pe>
2019-04-19 19:04:05 -0500

    New

--- forward.h
+++ forward.h
@@ -10,54 +10,97 @@ class ForwardList : public List<T> {
         ForwardList() : List<T>() {}
 
         T front() {
-            // TODO
+            return (this->head->data);
         }
 
         T back() {
-            // TODO
+            return (this->tail->data);
         }
 
         void push_front(T value) {
-            // TODO
+            Node<T> *temp = new Node<T>();
+            temp->data = value;
+            temp->next = this->head;
+            this->head = temp;
+            if (empty()) {
+                this->tail == temp;
+            }
+            ++this->nodes;
         }
 
         void push_back(T value) {
-            // TODO
+            Node<T> *temp = new Node<T>();
+            temp->data = value;
+            temp->next = NULL;
+
+            this->tail = temp;
+            if (empty())
+                this->head == temp;
+            ++this->nodes;
         }
 
         void pop_front() {
-            // TODO
+            if (!empty())
+            {
+                Node<T> *temp = this->head->next;
+                delete this->head;
+                this->head = temp;
+                --this->nodes;
+            }
         }
 
         void pop_back() {
-            // TODO
+            if (!empty())
+            {
+                Node<T> *temp = this->head;
+                while(temp->next != this->tail)
+                    temp = temp->next;
+                delete this->tail;
+                this->tail = temp;
+                this->tail->next = NULL;
+                --this->nodes;
+            }
         }
 
         T operator[](int index) {
-            // TODO
+            Node<T> *temp = new Node<T>();
+            temp = this->head;
+            for (int i = 0; i < index; i++)
+            {
+                if (temp->next == NULL)
+                    throw;
+                temp ->next = temp;
+            }
+            std::cout << temp << std::endl;
         }
 
         bool empty() {
-            // TODO
+            return (this->head == NULL ? true: false);
         }
 
         int size() {
-            // TODO
+            return this->nodes;
         }
 
         void clear() {
-            // TODO
+            Node<T> *temp = new Node<T>();
+            temp = this->tail;
+            for (int i = 0; i < size(); i++)
+            {
+                temp->next = temp;
+                delete temp;
+            }
         }
 
         void sort() {
             // TODO
         }
-    
+
         void reverse() {
             // TODO
         }
 
-        string name() {
+        std::string name() {
             return "Forward List";
         }
 
